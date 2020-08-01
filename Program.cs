2c997d5e7f481cdc5008f08a5bc1f2744cbe19ba
Jamie Holloway <jamie@jamieholloway.com>
2020-08-01 01:39:42 -0700

    sync

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace EmailSearch
         static void Main(string[] args)
         {
             var pattern = @"(?i)this is a hack";
-            string[] folders = { "Inbox", "todo", "fromJamie", "wip", "Deleted Items", "archive", "Sent Items", "AzureDevOps", "OpticalDRI", "bucket", "ICM" };
+            string[] folders = {"Inbox", "todo", "fromJamie", "wip", "Deleted Items", "archive", "Sent Items", "AzureDevOps", "OpticalDRI", "bucket", "ICM"};
 
             Console.BackgroundColor = ConsoleColor.DarkBlue;
             Console.ForegroundColor = ConsoleColor.White;
@@ -31,10 +31,11 @@ namespace EmailSearch
                         Console.WriteLine("null console input");
                         Environment.Exit(999);
                     }
+
                     break;
                 }
                 case 2:
-                    folders = new string[] { args[1] };
+                    folders = new string[] {args[1]};
                     Console.WriteLine($"searching only folder {args[1]}");
                     break;
                 default:
@@ -61,48 +62,60 @@ namespace EmailSearch
                 {
                     var outlookApplication = new Application();
                     var outlookNameSpace = outlookApplication.GetNamespace("MAPI");
-                    var folder = outlookNameSpace.Folders["jamieho@microsoft.com"].Folders[f];
-                    Console.WriteLine();
-                    Console.WriteLine($"Folder {f}");
-                    file.WriteLine($"<html><head></head><body><h1>{f}</h1></body></html>");
-
-                    foreach (dynamic mailItem in folder.Items)
+                    try
                     {
-                        var searchArea = "";
-                        Console.Write('.');
-                        if (!(mailItem is MailItem)) continue;
-                        try
-                        {
-                            searchArea = searchArea + mailItem?.To + " ";
-                            searchArea = searchArea + mailItem?.Sender.Name + " ";
-                            searchArea = searchArea + mailItem?.Subject + " ";
-                            searchArea = searchArea + mailItem?.Body + " ";
-                        }
-                        catch (System.Exception e)
-                        {
-                            Console.WriteLine();
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine(e.Message);
-                            Console.ForegroundColor = ConsoleColor.White;
-                        }
-                        var m = Regex.Match(searchArea, pattern, RegexOptions.Singleline | RegexOptions.IgnoreCase);
-                        if (!m.Success) continue;
+                        var folder = outlookNameSpace.Folders["jamieho@microsoft.com"].Folders[f];
                         Console.WriteLine();
-                        Console.WriteLine($"To: {mailItem.To} Subject: {mailItem.Subject}");
-                        var sb = new StringBuilder();
-                        sb.Append("<html><head><body>");
-                        sb.Append($@"<p><b>From:</b> {mailItem.SenderName}</p>");
-                        sb.Append($@"<p><b>To:</b> {mailItem.To}</p>");
-                        sb.Append($@"<p><b>Sent:</b> {mailItem.ReceivedTime}</p>");
-                        if (!string.IsNullOrEmpty(mailItem.CC)) sb.Append($@"<p><b>CC:</b> {mailItem.CC}</p>");
-                        if (!string.IsNullOrEmpty(mailItem.BCC)) sb.Append($@"<p><b>BCC:</b> {mailItem.BCC}</p>");
-                        sb.Append($@"<p><b>Subject:</b> {mailItem.Subject}</p>");
-                        sb.Append("</html></head></body>");
-                        file.WriteLine(sb);
-                        file.WriteLine(mailItem.HTMLBody);
-                        if (!notLaunched) continue;
-                        System.Diagnostics.Process.Start(outputFile);
-                        notLaunched = false;
+                        Console.WriteLine($"Folder {f}");
+                        file.WriteLine($"<html><head></head><body><h1>{f}</h1></body></html>");
+
+                        foreach (dynamic mailItem in folder.Items)
+                        {
+                            var searchArea = "";
+                            Console.Write('.');
+                            if (!(mailItem is MailItem)) continue;
+                            try
+                            {
+                                searchArea = searchArea + mailItem?.To + " ";
+                                searchArea = searchArea + mailItem?.Sender.Name + " ";
+                                searchArea = searchArea + mailItem?.Subject + " ";
+                                searchArea = searchArea + mailItem?.Body + " ";
+                            }
+                            catch (System.Exception e)
+                            {
+                                Console.WriteLine();
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine(e.Message);
+                                Console.ForegroundColor = ConsoleColor.White;
+                            }
+
+                            var m = Regex.Match(searchArea, pattern, RegexOptions.Singleline | RegexOptions.IgnoreCase);
+                            if (!m.Success) continue;
+                            Console.WriteLine();
+                            Console.WriteLine($"To: {mailItem.To} Subject: {mailItem.Subject}");
+                            var sb = new StringBuilder();
+                            sb.Append("<html><head><body>");
+                            sb.Append($@"<p><b>From:</b> {mailItem.SenderName}</p>");
+                            sb.Append($@"<p><b>To:</b> {mailItem.To}</p>");
+                            sb.Append($@"<p><b>Sent:</b> {mailItem.ReceivedTime}</p>");
+                            if (!string.IsNullOrEmpty(mailItem.CC)) sb.Append($@"<p><b>CC:</b> {mailItem.CC}</p>");
+                            if (!string.IsNullOrEmpty(mailItem.BCC)) sb.Append($@"<p><b>BCC:</b> {mailItem.BCC}</p>");
+                            sb.Append($@"<p><b>Subject:</b> {mailItem.Subject}</p>");
+                            sb.Append("</html></head></body>");
+                            file.WriteLine(sb);
+                            file.WriteLine(mailItem.HTMLBody);
+                            if (!notLaunched) continue;
+                            System.Diagnostics.Process.Start(outputFile);
+                            notLaunched = false;
+                        }
+                    }
+                    catch (System.Exception e)
+                    {
+                        Console.WriteLine();
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(e.Message);
+                        Console.ForegroundColor = ConsoleColor.White;
+                        continue;
                     }
                 }
             }
@@ -112,6 +125,7 @@ namespace EmailSearch
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine(e.ToString());
             }
+
             finally
             {
                 file.Close();
